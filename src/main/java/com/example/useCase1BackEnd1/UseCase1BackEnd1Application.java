3f54681dc2b246f52dc9e18e5307381c5e0b294f
Joycejlee00 <joycelee1294@gmail.com>
2023-05-18 16:12:01 -0400

    update pom.xml file and fix repository skeleton

---
+++ src/main/java/com/example/useCase1BackEnd1/UseCase1BackEnd1Application.java
@@ -0,0 +1,13 @@
+package com.example.useCase1BackEnd1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseCase1BackEnd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseCase1BackEnd1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/useCase1BackEnd1/UseCase1BackEnd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.useCase1BackEnd1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseCase1BackEnd1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
