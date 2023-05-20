fed86877f7d86e3b4160a3db618814b280b85dfb
Joycejlee00 <joycelee1294@gmail.com>
2023-05-20 17:49:31 -0400

    update Agent Repositor. Will Update this feature branch until basics of the skeleton for connection to DynamoDB

--- src/main/java/com/example/useCase1BackEnd1/repository/AgentRepository.java
+++ src/main/java/com/example/useCase1BackEnd1/repository/AgentRepository.java
@@ -1,4 +1,12 @@
 package com.example.useCase1BackEnd1.repository;
 
-public class AgentRepository {
+import com.example.useCase1BackEnd1.model.Agent;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+@EnableScan
+public interface AgentRepository extends CrudRepository<Agent, String> {
+    List<Agent> findByUser (String username);
+    List<Agent> findByID (String id);
 }
