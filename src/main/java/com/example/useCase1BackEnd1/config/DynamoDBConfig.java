8f5aec4eae85ac5c9aa08e0f4fd6eb7fb30409ef
Joycejlee00 <joycelee1294@gmail.com>
2023-05-22 14:25:15 -0400

    modify config file, add getter/setters for entities, only have the hashkey for repository and add service for crud operations

--- src/main/java/com/example/useCase1BackEnd1/config/DynamoDBConfig.java
+++ src/main/java/com/example/useCase1BackEnd1/config/DynamoDBConfig.java
@@ -1,8 +1,7 @@
 package com.example.useCase1BackEnd1.config;
 
 
-//Config File from https://github.com/derjust/spring-data-dynamodb
-//using derjust dependency to connect dynamodb
+//Config File from https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/creating-clients.html
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
@@ -12,45 +11,38 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.example.useCase1BackEnd1.repository.AgentRepository;
-import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableDynamoDBRepositories(basePackageClasses = AgentRepository.class)
-public class DynamoDBConfig {
-
-    @Value("${access_key}")
-    private String awsAccessKey;
-
-    @Value("${secret_access_key}")
-    private String awsSecretKey;
+class DynamoDBConfiguration {
 
     @Value("${endpoint}")
-    private String awsDynamoDBEndPoint;
+    private String endpoint;
 
     @Value("${region}")
-    private String awsRegion;
+    private String region;
 
+    @Value("${access_key}")
+    private String accessKey;
+
+    @Value("${secret_access_key}")
+    private String sercretAccessKey;
+
+    //https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBMapper.html
     @Bean
-    public AWSCredentials amazonAWSCredentials(){
-        return new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+    public DynamoDBMapper dynamoDBMapper() {
+        return new DynamoDBMapper(buildAmazonDynamoDB());
     }
-    public AWSCredentialsProvider amazonAWSCredentialsProvider(){
-        return new AWSStaticCredentialsProvider(amazonAWSCredentials());
-    }
-    // Returns the amazonDB instance using the endpoint as well as credentials
-    public AmazonDynamoDB amazonDynamoDB(){
-        return AmazonDynamoDBClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(awsDynamoDBEndPoint, awsRegion))
-                .withCredentials(amazonAWSCredentialsProvider())
+
+    private AmazonDynamoDB buildAmazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder
+                .standard()
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(endpoint,region))
+                .withCredentials(new AWSStaticCredentialsProvider(
+                        new BasicAWSCredentials(accessKey,sercretAccessKey)))
                 .build();
-    }
-
-    @Bean
-    public DynamoDBMapper mapper(){
-        return new DynamoDBMapper(amazonDynamoDB());
     }
 }
--- src/main/java/com/example/useCase1BackEnd1/repository/AgentRepository.java
+++ src/main/java/com/example/useCase1BackEnd1/repository/AgentRepository.java
@@ -3,10 +3,11 @@ package com.example.useCase1BackEnd1.repository;
 import com.example.useCase1BackEnd1.model.Agent;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
-import java.util.List;
+
+import java.util.Optional;
 
 @EnableScan
 public interface AgentRepository extends CrudRepository<Agent, String> {
-    List<Agent> findByUser (String username);
-    List<Agent> findByID (String id);
+//    List<Agent> findByUser (String username);
+    Optional<Agent> findById (String id);
 }
