5ff0b1a9404879d98783814e6eeaf2c4b1bdaae9
Joycejlee00 <joycelee1294@gmail.com>
2023-05-22 15:58:06 -0400

    add crud operations for agents (create, read, update, delete) in service class.

--- src/main/java/com/example/useCase1BackEnd1/service/CrudService.java
+++ src/main/java/com/example/useCase1BackEnd1/service/CrudService.java
@@ -2,10 +2,58 @@ package com.example.useCase1BackEnd1.service;
 
 
 //service class for crud operations
+//https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBMapper.Methods.html#DynamoDBMapper.Methods.save
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
+import com.example.useCase1BackEnd1.model.Agent;
+import com.example.useCase1BackEnd1.repository.AgentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CrudService {
 
+    @Autowired
+    DynamoDBMapper dynamoDBMapper;
+
+    @Autowired
+    AgentRepository agentRepository;
+
+
+    //add new agent
+    public Agent addAgent(Agent agent) {
+        dynamoDBMapper.save(agent);
+        return agent;
+    }
+
+    //get agent by id
+    public Agent getAgentsById(String agentId) {
+        return dynamoDBMapper.load(Agent.class, agentId);
+    }
+
+    //get all agents
+    public List<Agent> getAllAgents() {
+        List<Agent> retrieveAgents = dynamoDBMapper.scan(Agent.class, new DynamoDBScanExpression());
+        return retrieveAgents;
+    }
+
+    //Delete agent by id
+    public void deleteAgentById(String agentId) {
+        dynamoDBMapper.delete(dynamoDBMapper.load(Agent.class, agentId));
+    }
+
+    //update agent
+    public Agent updateAgent(Agent agent, String agentId) {
+        Optional<Agent> agentRepo = agentRepository.findById(agentId);
+
+        agentRepo.get().setKeyword(agent.getKeyword());
+        agentRepo.get().setPassword(agent.getPassword());
+        agentRepo.get().setUsername(agent.getUsername());
+
+        return agentRepository.save(agentRepo.get());
+    }
 }
